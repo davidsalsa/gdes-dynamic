4c27bd0140b7acd4c3651c26ffb427516b50a4f1
Jelle Dimmendaal <jellecf.1@gmail.com>
2019-10-03 10:30:44 +0200

    added switch script

---
+++ relief/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(BoxCollider))]
+public class SceneSwitcher : MonoBehaviour
+{
+    public string sceneToSwitchTo;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !string.IsNullOrEmpty(sceneToSwitchTo))
+        {
+            SceneManager.LoadScene(sceneToSwitchTo);
+        }
+    }
+}
