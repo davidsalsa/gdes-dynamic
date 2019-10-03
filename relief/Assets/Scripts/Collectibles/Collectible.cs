61733ecab803c691c8577c256e6fecec8d45c2b6
Jelle Dimmendaal <jellecf.1@gmail.com>
2019-10-03 09:52:15 +0200

    added collectible

---
+++ relief/Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Collectibles
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class Collectible : MonoBehaviour
+    {
+        private BoxCollider _boxCollider;
+
+        private void Start()
+        {
+            _boxCollider = GetComponent<BoxCollider>();
+            _boxCollider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
