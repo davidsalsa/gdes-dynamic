63c8ff0f348f9b8c5f836c9106ec733f15010019
Mike Lamsma <md.lamsma@student.han.nl>
2019-10-03 08:44:47 +0200

    Adds key animation

---
+++ relief/Assets/Scripts/KeyHover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class KeyHover : MonoBehaviour
+{
+    public float hoverFrequency;
+    public float hoverAmplitude;
+    public float rotationSpeed;
+    void Update()
+    {
+        // Apply hover effect
+        var tempPos = transform.position;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * hoverFrequency) * hoverAmplitude;
+        transform.position = tempPos;
+        
+        // Apply rotating effect
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotationSpeed);
+    }
+}
